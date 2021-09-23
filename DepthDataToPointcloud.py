c31aba9b4612ede6b4b186afb32de9aff671f226
Michal Polic <michal.polic@cvut.cz>
2021-09-23 14:07:52 +0200

    Speed up the conversion
    Speed up the conversion by better Python concatenation and adjust
    DepthDataToPointcloud to save PLY directly, i.e., skip objDir2Ply.

--- DepthDataToPointcloud.py
+++ DepthDataToPointcloud.py
@@ -4,12 +4,12 @@ import math
 import os
 
 np.set_printoptions(threshold=sys.maxsize)
-folder = sys.argv[1]
+folder = 'd:/tmp/ARTwinRecording/HoloLensRecording__2021_08_02__11_01_11_MUCLab_2/long_throw_depth'   #sys.argv[1]
 
 if not folder[-1] == '/':
     folder = folder + '/'
 
-uvdatafile = sys.argv[2]
+uvdatafile = "d:/tmp/ex_models/benchmark/uvdata.txt"   #sys.argv[2]
 
 uvfile = open(uvdatafile, 'r')
 uvdata = uvfile.read()
@@ -43,13 +43,32 @@ for r, d, f in os.walk(folder):
             values = np.reshape(values, (height, -1))
 
             #zpracovat na ply/obj
-            objfile = open(folder + timestamp + ".obj", 'w')
-            objfile.write("o Object.1\n")
+            # objfile = open(folder + timestamp + ".obj", 'w')
+            # objfile.write("o Object.1\n")
 
+            # for i in range(0, 450):
+            #     for j in range(0, 448):
+            #         r = values[i, j]
+            #         if not r == 0:
+            #             uv = uvdata["(" + str(j) + "," + str(i) + ")"]
+            #             u = uv[0]
+            #             v = uv[1]
+            #             d = r / math.sqrt(u * u + v * v + 1)
+            #             #print(d)
+            #             x = d * u
+            #             y = d * v
+            #             z = d * 1
+            #             objfile.write("v " + str(x) + " " + str(y) + " " + str(z) + "\n")
+            #             #print("Written to file")
+            # objfile.close()
+
+            out = open(folder + timestamp + ".ply", 'w')
+            vertices = []
+            numOfVerts = 0
             for i in range(0, 450):
                 for j in range(0, 448):
                     r = values[i, j]
-                    if not r == 0:
+                    if not r == 0 and "(" + str(j) + "," + str(i) + ")" in uvdata:
                         uv = uvdata["(" + str(j) + "," + str(i) + ")"]
                         u = uv[0]
                         v = uv[1]
@@ -57,9 +76,10 @@ for r, d, f in os.walk(folder):
                         #print(d)
                         x = d * u
                         y = d * v
-                        z = d * 1
-                        objfile.write("v " + str(x) + " " + str(y) + " " + str(z) + "\n")
-                        #print("Written to file")
+                        z = d * 1    
+                        vertices.append(str(x) + " " + str(y) + " " + str(z))
+                        numOfVerts = numOfVerts + 1
+            header = "ply\nformat ascii 1.0\nelement vertex " + str(numOfVerts) + "\nproperty float x\nproperty float y\nproperty float z\nend_header\n"
 
-
-            objfile.close()
+            out.write(header + "\n".join(vertices))
+            out.close()
--- HoloLensDataExtraction.py
+++ HoloLensDataExtraction.py
@@ -24,115 +24,115 @@ from PIL import Image
 recording_folders = []
 
 
-def connect(address, username, password):
-    print("Connecting to HoloLens Device Portal...")
-    url = "http://{}".format(address)
-    password_manager = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-    password_manager.add_password(None, url, username, password)
-    handler = urllib.request.HTTPBasicAuthHandler(password_manager)
-    opener = urllib.request.build_opener(handler)
-    opener.open(url)
-    urllib.request.install_opener(opener)
+# def connect(address, username, password):
+#     print("Connecting to HoloLens Device Portal...")
+#     url = "http://{}".format(address)
+#     password_manager = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+#     password_manager.add_password(None, url, username, password)
+#     handler = urllib.request.HTTPBasicAuthHandler(password_manager)
+#     opener = urllib.request.build_opener(handler)
+#     opener.open(url)
+#     urllib.request.install_opener(opener)
 
-    print("=> Connected to HoloLens at address:", url)
+#     print("=> Connected to HoloLens at address:", url)
 
-    response = urllib.request.urlopen(
-        "{}/api/app/packagemanager/packages".format(url))
-    packages = json.loads(response.read().decode())
+#     response = urllib.request.urlopen(
+#         "{}/api/app/packagemanager/packages".format(url))
+#     packages = json.loads(response.read().decode())
 
-    package_full_name = None
-    for package in packages["InstalledPackages"]:
-        if package["Name"] == "CV: Recorder":
-            package_full_name = package["PackageFullName"]
-            break
-    assert package_full_name is not None, \
-        "App not found"
+#     package_full_name = None
+#     for package in packages["InstalledPackages"]:
+#         if package["Name"] == "CV: Recorder":
+#             package_full_name = package["PackageFullName"]
+#             break
+#     assert package_full_name is not None, \
+#         "App not found"
 
-    print("=> Found application with name:",
-          package_full_name)
+#     print("=> Found application with name:",
+#           package_full_name)
 
-    print("Searching for recordings...")
+#     print("Searching for recordings...")
 
-    response = urllib.request.urlopen(
-        "{}/api/filesystem/apps/files?knownfolderid="
-        "LocalAppData&packagefullname={}&path=\\\\TempState".format(
-            url, package_full_name))
-    recordings = json.loads(response.read().decode())
+#     response = urllib.request.urlopen(
+#         "{}/api/filesystem/apps/files?knownfolderid="
+#         "LocalAppData&packagefullname={}&path=\\\\TempState".format(
+#             url, package_full_name))
+#     recordings = json.loads(response.read().decode())
 
-    recording_names = []
-    for recording in recordings["Items"]:
-        # Check if the recording contains any file data.
-        response = urllib.request.urlopen(
-            "{}/api/filesystem/apps/files?knownfolderid="
-            "LocalAppData&packagefullname={}&path=\\\\TempState\\{}".format(
-                url, package_full_name, recording["Id"]))
-        files = json.loads(response.read().decode())
-        if len(files["Items"]) > 0:
-            recording_names.append(recording["Id"])
-    recording_names.sort()
+#     recording_names = []
+#     for recording in recordings["Items"]:
+#         # Check if the recording contains any file data.
+#         response = urllib.request.urlopen(
+#             "{}/api/filesystem/apps/files?knownfolderid="
+#             "LocalAppData&packagefullname={}&path=\\\\TempState\\{}".format(
+#                 url, package_full_name, recording["Id"]))
+#         files = json.loads(response.read().decode())
+#         if len(files["Items"]) > 0:
+#             recording_names.append(recording["Id"])
+#     recording_names.sort()
 
-    print("=> Found a total of {} recordings".format(
-        len(recording_names)))
-    return url, recording_names, package_full_name
-
-
-def download_recordings(url, package_full_name, recordings, workspace_path):
-    for recording_name in recordings:
-        if recording_name is None:
-            return
-
-        recording_folders.append(recording_name)
-        recording_path = os.path.join(workspace_path, recording_name)
-        mkdir_if_not_exists(recording_path)
-
-        print("Downloading recording {}...".format(recording_name))
-
-        response = urllib.request.urlopen(
-            "{}/api/filesystem/apps/files?knownfolderid="
-            "LocalAppData&packagefullname={}&path=\\\\TempState\\{}".format(
-                url, package_full_name, recording_name))
-        files = json.loads(response.read().decode())
-
-        for file in files["Items"]:
-            if file["Type"] != 32:
-                continue
-
-            destination_path = os.path.join(recording_path, file["Id"])
-            if os.path.exists(destination_path):
-                print("=> Skipping, already downloaded:", file["Id"])
-                continue
-
-            print("=> Downloading:", file["Id"])
-            urllib.request.urlretrieve(
-                "{}/api/filesystem/apps/file?knownfolderid=LocalAppData&" \
-                "packagefullname={}&filename=\\\\TempState\\{}\\{}".format(
-                    url, package_full_name,
-                    recording_name, file["Id"]), destination_path)
+#     print("=> Found a total of {} recordings".format(
+#         len(recording_names)))
+#     return url, recording_names, package_full_name
 
 
-def delete_recordings(url, package_full_name, recordings):
-    for recording_name in recordings:
-        if recording_name is None:
-            return
+# def download_recordings(url, package_full_name, recordings, workspace_path):
+#     for recording_name in recordings:
+#         if recording_name is None:
+#             return
 
-        print("Deleting recording {}...".format(recording_name))
+#         recording_folders.append(recording_name)
+#         recording_path = os.path.join(workspace_path, recording_name)
+#         mkdir_if_not_exists(recording_path)
 
-        response = urllib.request.urlopen(
-            "{}/api/filesystem/apps/files?knownfolderid="
-            "LocalAppData&packagefullname={}&path=\\\\TempState\\{}".format(
-                url, package_full_name, recording_name))
-        files = json.loads(response.read().decode())
+#         print("Downloading recording {}...".format(recording_name))
 
-        for file in files["Items"]:
-            if file["Type"] != 32:
-                continue
+#         response = urllib.request.urlopen(
+#             "{}/api/filesystem/apps/files?knownfolderid="
+#             "LocalAppData&packagefullname={}&path=\\\\TempState\\{}".format(
+#                 url, package_full_name, recording_name))
+#         files = json.loads(response.read().decode())
 
-            print("=> Deleting:", file["Id"])
-            urllib.request.urlopen(urllib.request.Request(
-                "{}/api/filesystem/apps/file?knownfolderid=LocalAppData&" \
-                "packagefullname={}&filename=\\\\TempState\\{}\\{}".format(
-                    url, package_full_name,
-                    recording_name, file["Id"]), method="DELETE"))
+#         for file in files["Items"]:
+#             if file["Type"] != 32:
+#                 continue
+
+#             destination_path = os.path.join(recording_path, file["Id"])
+#             if os.path.exists(destination_path):
+#                 print("=> Skipping, already downloaded:", file["Id"])
+#                 continue
+
+#             print("=> Downloading:", file["Id"])
+#             urllib.request.urlretrieve(
+#                 "{}/api/filesystem/apps/file?knownfolderid=LocalAppData&" \
+#                 "packagefullname={}&filename=\\\\TempState\\{}\\{}".format(
+#                     url, package_full_name,
+#                     recording_name, file["Id"]), destination_path)
+
+
+# def delete_recordings(url, package_full_name, recordings):
+#     for recording_name in recordings:
+#         if recording_name is None:
+#             return
+
+#         print("Deleting recording {}...".format(recording_name))
+
+#         response = urllib.request.urlopen(
+#             "{}/api/filesystem/apps/files?knownfolderid="
+#             "LocalAppData&packagefullname={}&path=\\\\TempState\\{}".format(
+#                 url, package_full_name, recording_name))
+#         files = json.loads(response.read().decode())
+
+#         for file in files["Items"]:
+#             if file["Type"] != 32:
+#                 continue
+
+#             print("=> Deleting:", file["Id"])
+#             urllib.request.urlopen(urllib.request.Request(
+#                 "{}/api/filesystem/apps/file?knownfolderid=LocalAppData&" \
+#                 "packagefullname={}&filename=\\\\TempState\\{}\\{}".format(
+#                     url, package_full_name,
+#                     recording_name, file["Id"]), method="DELETE"))
 
 
 
@@ -169,24 +169,27 @@ def convert_images(folder):
 
 
 
-username = sys.argv[1]
-password = sys.argv[2]
-folder = sys.argv[3]
-if not folder[-1] == '/':
-    folder = folder + '/'
-IP = sys.argv[4]
-delAfter = sys.argv[5]
+# username = sys.argv[1]
+# password = sys.argv[2]
+# folder = sys.argv[3]
+# if not folder[-1] == '/':
+#     folder = folder + '/'
+# IP = sys.argv[4]
+# delAfter = sys.argv[5]
 
-mkdir_if_not_exists(folder)
+# mkdir_if_not_exists(folder)
 
-url, records, package_full_name = connect(IP, username, password)
-download_recordings(url, package_full_name, records, folder)
+# url, records, package_full_name = connect(IP, username, password)
+# download_recordings(url, package_full_name, records, folder)
 
-if delAfter == "1":
-    delete_recordings(url, package_full_name, records)
+# if delAfter == "1":
+#     delete_recordings(url, package_full_name, records)
 
 #extract tars
 sensors = ["pv", "vlc_ll", "vlc_lf", "vlc_rf", "vlc_rr", "long_throw_depth"]
+
+folder = "d:/tmp/ARTwinRecording/"
+recording_folders = ["building_third_floor_A__2020_11_16__14_17_39"]
 
 print("Extracting images")
 for recording in recording_folders:
